e1bc7680f4bad66fabac8dc69fab38d65b39fda1
Kyli Rouge <Kyli@BHStudios.org>
2016-02-10 22:14:15 -0500

    Started IntelliJ IDEA Project

---
+++ src/org/bh/tools/ui/delegation/BHTableView.java
@@ -0,0 +1,10 @@
+package org.bh.tools.ui.delegation;
+
+/**
+ * Copyright BHStudios ©2016 BH-1-PS. Made for BHTableView.
+ *
+ * @author Kyli Rouge
+ * @since 2016-02-10
+ */
+public class BHTableView {
+}
